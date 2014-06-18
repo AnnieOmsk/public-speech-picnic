a95bc898218a0fc40fb853ac494af2efb7c8585b
Dmitry Shmatko <dmitry.shmatko@7bits.it>
2014-06-18 16:02:21 +0700

    Admin branch cleared

--- api/services/TeasersService.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-exports.fetchTeasers = function(callback) {
-    http.get("http://ps.whereco.in/api/teasers", function(res) {
-        res.on('data', callback);
-    }).on('error', function(e) {
-        console.log("Got error: " + e.message);
-    });
-}
