c72c75c0db1c60d5f6d587f3f9069ce204143d22
Dmitry Shmatko <dmitry.shmatko@7bits.it>
2014-06-24 19:42:43 +0700

    Added animation to scrolling

--- assets/linker/js/slider.js
+++ assets/linker/js/slider.js
@@ -4,6 +4,8 @@ var ELEMENTS = ".social-container";
 var ITEMS = ".js-instagram-item";
 var SHOWED_CLASS = "active";
 var ITEMS_TO_SHOW = 4;
+var ITEM_WIDTH_IN_PERCENTS = 24;
+var ANIMATION_TIME = 250;
 
 
 $(function(){
@@ -12,6 +14,7 @@ $(function(){
         var firstFind = false;
         var activeOver = false;
         var first = {};
+        var last = {};
         for (i=items.length; i>0; i--) {
             if (items.eq(i).hasClass(SHOWED_CLASS)) {
                 if (!firstFind) {
@@ -21,15 +24,46 @@ $(function(){
             }
             if (firstFind && !items.eq(i).hasClass(SHOWED_CLASS) && !activeOver) {
                 activeOver = true;
-                items.eq(i).show();
-                items.eq(i).addClass(SHOWED_CLASS);
+                last = items.eq(i);
                 i = 0;
             }
         }
         if (activeOver) {
-            first.hide();
-            first.removeClass(SHOWED_CLASS);
+            last.css("margin-left", "-" + ITEM_WIDTH_IN_PERCENTS + "%");
+            last.show();
+            last.addClass(SHOWED_CLASS);
+            for (var i=ITEM_WIDTH_IN_PERCENTS-1; i>=0; i--) {
+                animate(i, last, first, true);
+            }
         }
+    };
+
+    var animate = function(i, last, first, left) {
+        var lastMargin;
+        var firstMargin;
+        if (left) {
+            lastMargin = "margin-left";
+            firstMargin = "margin-right";
+        } else {
+            lastMargin = "margin-right";
+            firstMargin = "margin-left";
+        }
+        var initialDelay = ANIMATION_TIME/ITEM_WIDTH_IN_PERCENTS;
+        var delayStep = initialDelay/ITEM_WIDTH_IN_PERCENTS;
+        var j = ITEM_WIDTH_IN_PERCENTS - i;
+        var delay = initialDelay*(j+1) + delayStep*j*(j+1)/2;
+        setTimeout(function() {
+            if (i!=0) {
+                last.css(lastMargin, "-" + i + "%");
+                first.css(firstMargin, "-" + (ITEM_WIDTH_IN_PERCENTS-i) + "%");
+            }
+            if (i==0) {
+                last.css(lastMargin, "");
+                first.css(firstMargin, "");
+                first.hide();
+                first.removeClass(SHOWED_CLASS);
+            }
+        }, delay);
     };
 
     var scrollRight = function(event) {
@@ -37,6 +71,7 @@ $(function(){
         var firstFind = false;
         var activeOver = false;
         var first = {};
+        var last = {};
         for (i=0; i<items.length; i++) {
             if (items.eq(i).hasClass(SHOWED_CLASS)) {
                 if (!firstFind) {
@@ -46,14 +81,17 @@ $(function(){
             }
             if (firstFind && !items.eq(i).hasClass(SHOWED_CLASS) && !activeOver) {
                 activeOver = true;
-                items.eq(i).show();
-                items.eq(i).addClass(SHOWED_CLASS);
+                last = items.eq(i);
                 i = items.length;
             }
         }
         if (activeOver) {
-            first.hide();
-            first.removeClass(SHOWED_CLASS);
+            last.css("margin-right", "-" + ITEM_WIDTH_IN_PERCENTS + "%");
+            last.show();
+            last.addClass(SHOWED_CLASS);
+            for (var i=ITEM_WIDTH_IN_PERCENTS-1; i>=0; i--) {
+                animate(i, last, first, false);
+            }
         }
     };
 
