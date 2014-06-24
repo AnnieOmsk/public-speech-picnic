b24c15f20ce79b4ccedcb6473545a0f048f5c213
Dmitry Shmatko <dmitry.shmatko@7bits.it>
2014-06-24 15:16:05 +0700

    Added links to likes and comments for instagram

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -33,7 +33,7 @@ module.exports = {
           timelineService.findTimelines(),
           broadcastService.findBroadcasts(),
           twitterService.findTweets('#пикник', 50),
-          instagramService.findInstagrams('пикник', 4)
+          instagramService.findInstagrams('пикник', 5)
       ]);
       allPromise.then(function(data){
           return res.view({
