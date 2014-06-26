beede841d6842e3c45e9b87b6c4f8752453fbbbc
K.Kolotyuk <kk@7bits.com>
2014-06-26 15:38:36 +0700

    Beta for translation

--- assets/linker/js/translation.js
+++ assets/linker/js/translation.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
+    var RELOAD_BROADCAST = '.js-reload-broadcast';
+    var DATA_URL = "data-url";
+    var CONTAINER = ".js-wrap-broadcast"
+
+    var reload = function(event) {
+        var url = event.target.attributes[DATA_URL].value;
+        var source   = $("#broadcast-template").html();
+        var template = Handlebars.compile(source);
+        $(CONTAINER).empty();
+        $.get(url, 'json')
+            .done(function(data){
+                drawBroadcast({broadcast: data}, CONTAINER, template);
+            })
+            .fail(function(jqXHR, textStatus) {
+                console.log("fail:" + textStatus);
+            });
+        return false;
+    };
+
+    var drawBroadcast = function(data, container, template) {
+        var html = template(data);
+        $(container).html(html);
+    }
+
+    $(RELOAD_BROADCAST).on("click", reload);
+
   $(document).on('click', '.js-news-more', function () {
       $(this).addClass('hide');
       $(this).parent().siblings('.js-news-full').removeClass('hide');
