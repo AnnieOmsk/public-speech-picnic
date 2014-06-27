416cd973139e50ac6630d03fc205bfe299bc4984
Pavel Baybara <baybara.pavel@gmail.com>
2014-06-27 11:42:46 +0700

    Added fixed nav

---
+++ assets/linker/js/sticky-header.js
@@ -0,0 +1,19 @@
+// $(document).ready(function () {
+//   var stickyTop = $('.js-promo').offset().top;
+//   var stickyNav = $('.js-social-news').offset().top;
+          
+//         $(window).scroll(function(){
+//                 if( $(window).scrollTop() > stickyTop ) {
+//                         // $('.js-nav-static').addClass('hide');
+//                         $('.js-nav-fixed').removeClass('hide');
+//                         if ($(window).scrollTop() >= stickyNav ) {
+//                           $('.js-nav-fixed').addClass('fixed');
+//                         } else {
+//                           $('.js-nav-fixed').removeClass('fixed');
+//                         }
+//                 } else {
+//                         // $('.js-nav-static').removeClass('hide');
+//                         $('.js-nav-fixed').addClass('hide');
+//                 }
+//         });
+// });
