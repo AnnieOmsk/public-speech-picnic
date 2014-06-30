79302f246112087a7cbb4906144e79a479c4a3cc
K.Kolotyuk <kk@7bits.com>
2014-06-30 09:51:17 +0700

    Baloon fixes

--- assets/linker/js/popover.js
+++ assets/linker/js/popover.js
@@ -1,3 +1,9 @@
 $(function(){
-    $('.js-what-is-it').popover();
+    $('.js-what-is-it').popover({
+        content: 'Это первый городской праздник организованный горожанами, без администраций, организаций и корпораций. ' +
+            'Это живой концерт лучших омских групп, десятки выставок и перфомансов, игры, лекции и кино под открытым небом. ' +
+            'И все это омичи делают сами для себя.',
+        template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>',
+        placement: 'top'
+    });
 });
