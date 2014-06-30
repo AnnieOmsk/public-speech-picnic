d354685e3074bd36d5c0259f7a736b8bd1d97e72
Pavel Baybara <baybara.pavel@gmail.com>
2014-06-30 12:07:04 +0700

    Changed time interval

--- assets/linker/js/popover.js
+++ assets/linker/js/popover.js
@@ -3,7 +3,8 @@ $(function(){
         content: 'Это первый городской праздник организованный горожанами, без администраций, организаций и корпораций. ' +
             'Это живой концерт лучших омских групп, десятки выставок и перфомансов, игры, лекции и кино под открытым небом. ' +
             'И все это омичи делают сами для себя.',
-        template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>',
+        template: '<div class="popover" role="tooltip"><div class="popover-content"></div></div>',
         placement: 'top'
+        // <div class="arrow"></div>
     });
 });
