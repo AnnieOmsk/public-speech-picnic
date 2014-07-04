acda3d313af587469d94009d403da5c8a1966e39
Pavel Baybara <baybara.pavel@gmail.com>
2014-07-04 18:34:22 +0700

    Ccompresed images

--- assets/linker/js/popover.js
+++ assets/linker/js/popover.js
@@ -1,11 +1,19 @@
-$(function(){
-    $('.js-what-is-it').popover({
-        content: 'Это первый городской праздник организованный горожанами, без администраций, организаций и корпораций. ' +
-            'Это живой концерт лучших омских групп, десятки выставок и перфомансов, игры, лекции и кино под открытым небом. ' +
-            'И все это омичи делают сами для себя.',
-        template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>',
-        placement: 'top',
-        animation: true
-        // 
-    });
+$(document).ready(function() {
+  $(function(){
+      $('.js-what-is-it').popover({
+          content: 'Это первый городской праздник организованный горожанами, без администраций, организаций и корпораций. ' +
+              'Это живой концерт лучших омских групп, десятки выставок и перфомансов, игры, лекции и кино под открытым небом. ' +
+              'И все это омичи делают сами для себя.',
+          template: '<div class="popover " role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>',
+          placement: 'top',
+          animation: true
+          // 
+      });
+      $(document).on('click', '.popover', function() {
+        $('.js-what-is-it').popover('hide');
+      });
+      $(document).on('touch', '.popover', function() {
+        $('.js-what-is-it').popover('hide');
+      });
+  });
 });
